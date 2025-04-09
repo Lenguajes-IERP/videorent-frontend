d74d2efdd86dd29d8e436cce787260874dfeb33b
unknown <alvamen@gmail.com>
2025-04-09 10:55:46 -0600

    PeliculaInsertComponent

--- src/app/services/genero.service.ts
+++ src/app/services/genero.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Genero } from '../domain/genero.model';
-import { environment } from '../../environments/environment';
-import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
